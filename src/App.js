6272c939e99e01259b05e1b0bdd8edab211291e0
MohmedIkram <53212527+MohmedIkram@users.noreply.github.com>
2021-07-30 15:29:59 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
+import "./style.css";
+import NavBarComp from "./components/NavbarComp.js";
+import { Header } from "./components/Header.js";
+import { CardComp } from "./components/CardComp.js";
+import { Footer } from "./components/Footer.js";
+import { useState } from "react";
 
+function App() {
+  const [item, setItem] = useState([]);
+
+  return (
+    <div className="App">
+      <NavBarComp item={item} />
+      <Header />
+      <CardComp setItem={setItem} item={item} />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
